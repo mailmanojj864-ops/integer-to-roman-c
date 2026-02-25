66766c5f90ea10fad5466d5a6a6e6da95278ca21
Manoj <mailmanojj864@gmail.com>
2026-02-25 20:34:59 +0530

    Add files via upload

---
+++ solution.c
@@ -0,0 +1,19 @@
+char* intToRoman(int num) {
+    // Roman symbols and their corresponding values
+    int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    char *symbols[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL",
+                       "X", "IX", "V", "IV", "I"};
+    
+    // Allocate enough memory (max Roman length is < 20 for 3999)
+    char *result = (char *)malloc(20 * sizeof(char));
+    result[0] = '\0';  // Initialize as empty string
+    
+    for (int i = 0; i < 13; i++) {
+        while (num >= values[i]) {
+            strcat(result, symbols[i]);
+            num -= values[i];
+        }
+    }
+    
+    return result;
+}
